dab006cc8ce9556f9bbd137b52389391818d66a8
affggh <879632264@qq.com>
2023-09-10 21:02:29 +0800

    rk3566: Fix missing packages

--- device.mk
+++ device.mk
@@ -65,7 +65,7 @@ PRODUCT_COPY_FILES += \
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt 
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
 
 # Vendor missing Minijail
 PRODUCT_PACKAGES += \
@@ -81,18 +81,55 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-service.vendor \
+    android.hardware.health@2.0-impl-2.1.vendor
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-service.vendor \
+    android.hardware.bluetooth@1.0-impl.vendor \
+
+
+# Media
+PRODUCT_PACKAGES += \
+    libstagefrighthw.vendor
+
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-external-service.vendor \
+    android.hardware.camera.provider@2.4-service.vendor \
+    android.hardware.camera.provider@2.4-external.vendor \
+    android.hardware.camera.provider@2.4-legacy.vendor \
+    android.hardware.camera.provider@2.4-impl.vendor \
+
 # DRM
 PRODUCT_PACKAGES += \
-    libmockdrmcryptoplugin
+    libmockdrmcryptoplugin \
 
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.3-service.clearkey.vendor \
+    android.hardware.drm@1.3-service.widevine.vendor
 
+# Graphics
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey \
-    android.hardware.drm@1.3-service.widevine
+    android.hardware.graphics.allocator@4.0-service.vendor \
+    android.hardware.graphics.composer@2.1-service.vendor \
+    gralloc.default.vendor \
+
+# Power
+PRODUCT_PACKAGES += \
+    power.default.vendor \
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    vibrator.default.vendor
+
+# HW_OUTPUT
+PRODUCT_PACKAGES += \
+    hw_output.default.vendor
 
 # Keymaster
 PRODUCT_PACKAGES += \
@@ -101,7 +138,9 @@ PRODUCT_PACKAGES += \
     libkeymaster_portable.vendor:64 \
     libkeymaster_messages.vendor:64 \
     libsoft_attestation_cert.vendor:64 \
-    libpuresoftkeymasterdevice.vendor:64
+    libpuresoftkeymasterdevice.vendor:64 \
+    libkeystore-engine-wifi-hidl.vendor:64 \
+    libkeystore-wifi-hidl.vendor:64 \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
@@ -113,7 +152,59 @@ PRODUCT_PACKAGES += \
     init.rk356x.rc \
     init.rockchip.rc \
     fstab.rk30board \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+
+# Stage
+PRODUCT_PACKAGES += \
+    libstagefright_amrnb_common.vendor:32 \
+    libstagefright_enc_common.vendor:32 \
+    libstagefright_flacdec.vendor:32 \
+    libstagefright_softomx.vendor:32 \
+    libstagefright_softomx_plugin.vendor:32 \
+    libstagefright_soft_aacdec.vendor:32 \
+    libstagefright_soft_aacenc.vendor:32 \
+    libstagefright_soft_amrdec.vendor:32 \
+    libstagefright_soft_amrnbenc.vendor:32 \
+    libstagefright_soft_amrwbenc.vendor:32 \
+    libstagefright_soft_avcdec.vendor:32 \
+    libstagefright_soft_avcenc.vendor:32 \
+    libstagefright_soft_flacdec.vendor:32 \
+    libstagefright_soft_flacenc.vendor:32 \
+    libstagefright_soft_g711dec.vendor:32 \
+    libstagefright_soft_gsmdec.vendor:32 \
+    libstagefright_soft_hevcdec.vendor:32 \
+    libstagefright_soft_mp3dec.vendor:32 \
+    libstagefright_soft_mpeg4dec.vendor:32 \
+    libstagefright_soft_mpeg4enc.vendor:32 \
+    libstagefright_soft_opusdec.vendor:32 \
+    libstagefright_soft_rawdec.vendor:32 \
+    libstagefright_soft_vorbisdec.vendor:32 \
+    libstagefright_soft_vpxdec.vendor:32 \
+    libstagefright_soft_vpxenc.vendor:32 \
+    libstagefright_soft_mpeg2dec.vendor:32
+
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-service.vendor \
+    android.hardware.sensors@1.0-impl.vendor
+
+# Crypto
+PRODUCT_PACKAGES += \
+    libboringssl.vendor
+
+# Misc
+PRODUCT_PACKAGES += \
+    baseparameter.vendor \
+    libchrome.vendor \
+    libgralloc_priv_omx.vendor \
+    libprotobuf-cpp-full-3.9.1.vendor \
+    libprotobuf-cpp-lite-3.9.1.vendor \
+    libopus.vendor:32 \
+    libvorbisidec.vendor:32 \
+    libvpx.vendor:32 \
+    libwebrtc_audio_coding.vendor
+
 
 # Seccomp
 PRODUCT_COPY_FILES += \
@@ -129,6 +220,23 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_rtk.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_rtk.conf \
+
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service.vendor
+
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio.service.vendor \
+    android.hardware.audio.common-util.vendor \
+    android.hardware.audio.common@6.0-util.vendor \
+    android.hardware.audio.effect@6.0-impl.vendor \
+    android.hardware.audio@6.0-impl.vendor \
+    audio.primary.default.vendor \
+    audio.r_submix.default.vendor \
+    audio.usb.default.vendor \
+    libaudiopreprocessing.vendor \
+    android.hardware.graphics.composer@2.1-resources.vendor:64
+
 
 # VNDK
 PRODUCT_PACKAGES += \
